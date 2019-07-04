7a027f11a601dc5396d1367f2716b79ea9fa7338
dimitri <D.M.Stallenberg@student.tudelft.nl>
2019-07-04 12:47:20 +0200

    Create replace language node

--- TOB/src/main/java/nl/tudelft/tobuilder/lang/PairReplaceNode.java
+++ TOB/src/main/java/nl/tudelft/tobuilder/lang/PairReplaceNode.java
@@ -34,6 +34,8 @@ public class PairReplaceNode extends ReplaceNode {
         this.targets = targets;
         this.follow = follow;
         this.injection = injection;
+
+        System.out.println(targets.toString());
     }
 
     @Override
@@ -42,27 +44,37 @@ public class PairReplaceNode extends ReplaceNode {
 
         List<List<Pair<String, String>>> permutations = new ArrayList<>();
 
+        System.out.println(strings.toString());
 
+        for (int i = 0; i < strings.size() - 2; i++) {
+            List<Pair<String, String>> permutation = new ArrayList<>();
 
+            String key = strings.get(i).getFirst();
+            String closeTag = targets.get(key).split(key)[1];
+            String value = key + closeTag + getSymbol();
 
-//
-//        for (int i = 0; i < strings.size(); i++) {
-//            for (int j = i + 1; j < strings.size(); j++) {
-//                List<Pair<String, String>> permutation = new ArrayList<>();
-//
-//                for (int k = 0; k < strings.size(); k++) {
-//                    if (i == k) {
-//                        permutation.add(new Pair<>(strings.get(k).getFirst(), getSymbol()));
-//                    } else if (j == k) {
-//                        permutation.add(new Pair<>(strings.get(k).getFirst(), getFollow()));
-//                    } else {
-//                        permutation.add(new Pair<>(strings.get(k).getFirst(), strings.get(k).getFirst()));
-//                    }
-//                }
-//
-//                permutations.add(permutation);
-//            }
-//        }
+            String keyReplacement = strings.get(i + 1).getFirst();
+            String targetReplacement = targets.get(keyReplacement);
+
+            String keyFinal = strings.get(i + 2).getFirst();
+            String openTag = targets.get(keyFinal).split(keyFinal)[0];
+
+            String valueFinal = getFollow() + targetReplacement.split(keyReplacement)[0] + injection + targetReplacement.split(keyReplacement)[1] + openTag + keyFinal;
+
+            for (int j = 0; j < strings.size(); j++) {
+                if (i == j) {
+                    permutation.add(new Pair<>(strings.get(j).getFirst(), value));
+                } else if (i + 1 == j) {
+                    permutation.add(new Pair<>(strings.get(j).getFirst(), strings.get(j).getFirst())); // Stays the same
+                } else if (i + 2 == j) {
+                    permutation.add(new Pair<>(strings.get(j).getFirst(), valueFinal));
+                } else {
+                    permutation.add(new Pair<>(strings.get(j).getFirst(), strings.get(j).getFirst())); // Anything else stays the same as well.
+                }
+            }
+            System.out.println(permutation.toString());
+            permutations.add(permutation);
+        }
 
         return permutations;
     }
